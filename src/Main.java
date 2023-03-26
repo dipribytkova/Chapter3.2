c1fab3d084c084391bb3efc1eb42218007fb8e1e
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-03-26 23:05:17 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int initially = 100;
+        int replenishment = 400;
+        int bet = 100;
+        int bonus = 1;
+
+        if (replenishment > 1000) {
+            System.out.println((initially / bet * bonus) + " бонус(ов)");
+        } else {
+            System.out.println((initially + replenishment) + " бонус(ов)");
+        }
     }
+
 }
